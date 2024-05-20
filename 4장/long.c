33139b3fa766653fc1abf0f28e2ca3aa6f3c8b85
Sossoh <128332587+Sossoh@users.noreply.github.com>
2024-05-20 23:35:14 +0900

    Update long.c

--- 4장/long.c
+++ 4장/long.c
@@ -1,30 +1,38 @@
  #include <stdio.h>
- #define MAXLINE 100
- void copy(char from[], char to[]);
- char line[MAXLINE]; // 입력줄
-char longest[MAXLINE]; // 가장긴줄
-/*입력줄가운데가장긴줄프린트*/
- main()
- {
- int len;
- int max;
- max = 0;
- while (gets(line) != NULL) {
- len = strlen(line);
- if (len > max) {
- max = len;
- copy(line, longest);
- }
- } if (max > 0) // 입력줄이있었다면
-printf("%s", longest);
- return 0;
- }
- /* copy: from을to에복사; to가충분히크다고가정*/
- void copy(char from[], char to[])
- {
- int i;
- i = 0;
- while ((to[i] = from[i]) != '\0')
- ++i;
- }
-/*단일모듈프로그램*/
+#include <string.h>
+
+#define MAXLINE 100
+
+void copy(char from[], char to[]);
+
+char line[MAXLINE];    // 입력 줄
+char longest[MAXLINE]; // 가장 긴 줄
+
+/* 입력 줄 중에서 가장 긴 줄 프린트 */
+int main() {
+    int len;
+    int max;
+
+    max = 0;
+    while (fgets(line, MAXLINE, stdin) != NULL) {
+        len = strlen(line);
+        if (len > max) {
+            max = len;
+            copy(line, longest);
+        }
+    }
+
+    if (max > 0) // 입력 줄이 있었다면
+        printf("%s", longest);
+
+    return 0;
+}
+
+/* copy: from을 to에 복사; to가 충분히 크다고 가정 */
+void copy(char from[], char to[]) {
+    int i;
+    i = 0;
+
+    while ((to[i] = from[i]) != '\0')
+        ++i;
+}
