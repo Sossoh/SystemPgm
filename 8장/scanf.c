275236d3ba73c2d918699981f0fca93adc70309b
Sossoh <128332587+Sossoh@users.noreply.github.com>
2024-05-21 18:08:41 +0900

    Create scanf.c

---
+++ 8장/scanf.c
@@ -0,0 +1,10 @@
+int main(){
+        int numinput;
+        char charinput;
+        scanf("%d",&numinput);
+        scanf("%d\n",numinput);
+        scanf("%c",&charinput);
+        iscanf("%c",charinput);
+        return 0;
+}
+~   
